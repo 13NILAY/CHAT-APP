acbf9e277a12aea5e584230054a90e1c4af25824
13NILAY <nilayrathod1303@gmail.com>
2024-11-19 08:22:26 +0530

    ErrorSolved

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,17 +1,18 @@
-const allowedOrigins = require('./allowedOrigins');
-
-const corsOptions = {
+const allowedOrigins = [
+    "https://onechatflow.netlify.app",
+    "http://localhost:5173",              // For local testing
+  ];
+  
+  const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials:true,
-    allowedHeaders: ['Authorization', 'Content-Type','authorization'], // Allow Authorization header
-    optionsSuccessStatus: 200
-}
-
-module.exports = corsOptions;
+    credentials: true, // Allows cookies and auth headers
+  };
+  
+  module.exports = corsOptions;
+  
